7cd2b34293775b6c756daf1944d424d8f74092c6
mukhibillah <mukhib.cascuser303@gmail.com>
2023-09-12 01:55:40 +0700

    Initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">Happy Birhtday Bund!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">HAPPY BIRTHDAY ru!</h3>
+      <p>
+        Mosi mosi bund, selamat ulang tahun ya bun. semoga kamu selalu di
+        berkahi oleh Allah SWT. jangan lupa buat selal tersenyum dan bersyukur
+        :)
+      </p>
+      <p>your hubby</p>
+    </div>
+  </div>
+);
